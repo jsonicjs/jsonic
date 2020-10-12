e82835a09ff8aa67888065b57af6f71ac8527083
Oliver Sturm <oliver@oliversturm.com>
2020-10-12 13:08:36 +0100

    fixing .. for .

--- jsonic-cmd.js
+++ jsonic-cmd.js
@@ -1,4 +1,4 @@
-const jsonic = require('../jsonic');
+const jsonic = require('./jsonic');
 const json = process.argv.slice(2).join(' ');
 const obj = jsonic(json);
 console.log(JSON.stringify(obj));
