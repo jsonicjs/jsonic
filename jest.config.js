19defe0e347b4f858ac9a7b54799230a9a2a2b9b
Richard Rodger <richard@ricebridge.com>
2022-09-30 17:07:04 +0100

    move to ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  // preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.js'],
   testTimeout: 9999,
-  // collectCoverageFrom: ['*.js'],
   coveragePathIgnorePatterns: ['test'],
   transform: {
-    "^.+\\.tsx?$": ["esbuild-jest",{sourcemap:true}]
+    "^.+\\.tsx?$": ["es-jest",{sourcemap:true}]
   },
 };
