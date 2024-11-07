2bd2f399fa00e4d3ca51418db90b8e1289e7eaf2
Richard Rodger <richard@ricebridge.com>
2024-11-07 16:47:38 +0000

    release-name

--- test/test-plugins.sh
+++ test/test-plugins.sh
@@ -1,42 +1,42 @@
 # Assumes plugin repos checked out at same level
 
 cd ../path
-npm link @jsonic/jsonic-next
+npm link jsonic
 npm run build
 npm test
 
 cd ../directive
-npm link @jsonic/jsonic-next
+npm link jsonic
 npm run build
 npm test
 
 cd ../multisource
-npm link @jsonic/jsonic-next
+npm link jsonic
 npm run build
 npm test
 
 cd ../hoover
-npm link @jsonic/jsonic-next
+npm link jsonic
 npm run build
 npm test
 
 cd ../expr
-npm link @jsonic/jsonic-next
+npm link jsonic
 npm run build
 npm test
 
 cd ../csv
-npm link @jsonic/jsonic-next
+npm link jsonic
 npm run build
 npm test
 
 cd ../toml
-npm link @jsonic/jsonic-next
+npm link jsonic
 npm run build
 npm test
 
 cd ../ini
-npm link @jsonic/jsonic-next
+npm link jsonic
 npm run build
 npm test
 
