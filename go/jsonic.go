895ef456aa914a9a9bebd82a2b5442357b51e8fb
Richard Rodger <richard@ricebridge.com>
2026-04-07 13:45:16 +0100

    go: v0.1.7

--- go/jsonic.go
+++ go/jsonic.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Version is the current version of the jsonic Go module.
-const Version = "0.1.6"
+const Version = "0.1.7"
 
 // Error message templates matching TypeScript defaults.
 var errorMessages = map[string]string{
