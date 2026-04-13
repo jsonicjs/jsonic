b9f78cab92c77e83d6e614eca9f79a3614ef0359
Richard Rodger <richard@ricebridge.com>
2026-04-13 16:02:19 +0100

    go: v0.1.14

--- go/README.md
+++ go/README.md
@@ -1,6 +1,6 @@
 # jsonic (Go)
 
-Version: 0.1.13
+Version: 0.1.14
 
 A Go port of [jsonic](https://github.com/jsonicjs/jsonic), the lenient
 JSON parser. Same architecture, same syntax, same results. If you
--- go/jsonic.go
+++ go/jsonic.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Version is the current version of the jsonic Go module.
-const Version = "0.1.13"
+const Version = "0.1.14"
 
 // Error message templates matching TypeScript defaults.
 var errorMessages = map[string]string{
